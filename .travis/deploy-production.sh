aad501f914513be4cefb5f75e6cc35894eda628b
Raymon de Looff <raydelooff@gmail.com>
2016-04-20 12:02:14 +0200

    Fix reference to deploy key

--- .travis/deploy-production.sh
+++ .travis/deploy-production.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 eval "$(ssh-agent -s)"
-chmod 600 deploy-key.pem
-ssh-add deploy-key.pem
+chmod 600 .travis/deploy-key.pem
+ssh-add .travis/deploy-key.pem
 git fetch --unshallow
 git remote add deploy ssh://root@imc.clowting.me:/root/imc-retourmanagement.git
 git push deploy +master:refs/heads/master
