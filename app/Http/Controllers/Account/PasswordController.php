57f893645d4a2794a77de79b97ef32a14751eccd
Raymon de Looff <raydelooff@gmail.com>
2016-04-26 11:31:29 +0200

    Validate current password on password change

--- app/Http/Controllers/Account/PasswordController.php
+++ app/Http/Controllers/Account/PasswordController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Account;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laracasts\Flash\Flash;
+use Validator;
 
 class PasswordController extends Controller
 {
@@ -16,16 +18,31 @@ class PasswordController extends Controller
 
     public function update(Request $request)
     {
-        $this->validate($request, [
+        $user = Auth::user();
+
+        $validator = Validator::make($request->all(), [
             'password_current' => 'required',
             'password' => 'required|confirmed|min:6',
         ]);
+
+        // Validate current password
+        $validator->after(function($validator) use ($request, $user) {
+            if(!Hash::check($request->input('password_current'), $user->password)) {
+                $validator->errors()->add('password_current', 'huidige wachtwoord is onjuist.');
+            }
+        });
+
+        if($validator->fails()) {
+            return redirect(route('account.password.edit'))
+                ->withErrors($validator)
+                ->withInput();
+        }
 
         $credentials = $request->only(
             'password'
         );
 
-        $user = Auth::user();
+        // Save new password
         $user->password = bcrypt($credentials['password']);
         $user->save();
 
