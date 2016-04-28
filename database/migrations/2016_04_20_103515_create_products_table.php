d651e8e41fff8fe53e330fe86e80afb4d2372426
carlogeertse <carlogeertse@hotmail.com>
2016-04-28 13:02:10 +0200

    Change database stucture with migrations

--- database/migrations/2016_04_20_103515_create_products_table.php
+++ database/migrations/2016_04_20_103515_create_products_table.php
@@ -16,21 +16,21 @@ class CreateProductsTable extends Migration
             Schema::create('products', function(Blueprint $table) {
                 $table->increments('id');
                 $table->string('name');
-$table->string('description');
-$table->string('short_description');
-$table->bigInteger('ean_code');
-$table->string('serial_number');
-$table->bigInteger('invoice_number');
-$table->integer('weight');
-$table->string('country_code');
-$table->string('location');
-$table->string('supplier');
-$table->string('quality_label');
-$table->string('status');
-$table->string('tax_group');
-$table->string('problem_description');
-$table->string('color');
-$table->float('msrp');
+                $table->string('description');
+                $table->string('short_description');
+                $table->bigInteger('ean_code');
+                $table->string('serial_number');
+                $table->bigInteger('invoice_number');
+                $table->integer('weight');
+                $table->string('country_code');
+                $table->string('location');
+                $table->string('supplier');
+                $table->string('quality_label');
+                $table->string('status');
+                $table->string('tax_group');
+                $table->string('problem_description');
+                $table->string('color');
+                $table->float('msrp');
 
                 $table->timestamps();
             });
