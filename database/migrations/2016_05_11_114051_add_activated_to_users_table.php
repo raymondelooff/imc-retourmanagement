4d43af4606e444899ef721b9f6e9d73571ab183f
Raymon de Looff <raydelooff@gmail.com>
2016-05-11 16:05:37 +0200

    Migration fix, drop column instead of creating it

--- database/migrations/2016_05_11_114051_add_activated_to_users_table.php
+++ database/migrations/2016_05_11_114051_add_activated_to_users_table.php
@@ -25,7 +25,7 @@ class AddActivatedToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('activated')->default(0);
+            $table->dropColumn('activated');
         });
     }
 }
