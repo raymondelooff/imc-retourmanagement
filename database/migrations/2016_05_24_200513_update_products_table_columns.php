66dd57c7c4b8e1dbe9a538d5c640f8aa15caeb99
carlogeertse <carlogeertse@hotmail.com>
2016-05-24 22:24:07 +0200

    Final fixes to product creation/editing

---
+++ database/migrations/2016_05_24_200513_update_products_table_columns.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProductsTableColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function($table){
+            $table->string('ean_code',20)->change();
+            $table->string('invoice_number',20)->change();
+            $table->string('weight',11)->change();
+            $table->string('msrp',12)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function($table){
+            $table->bigInteger('ean_code',20)->change();
+            $table->bigInteger('invoice_number',20)->change();
+            $table->integer('weight',11)->change();
+            $table->decimal('msrp',10,2)->change();
+        });
+    }
+}
